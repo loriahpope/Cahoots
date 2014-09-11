61ea9f998b4b7fab3c29bf00adf0d18798024196
Loriah Pope <loriahpope@172-26-126-33.dynapool.nyu.edu>
2014-09-11 18:55:12 -0400

    Final Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var prompt = require('sync-prompt').prompt;
 
 var generateCards = function(){
-	
+
 	var deck = [];
 	
 	//generate number cards
@@ -15,86 +15,77 @@ var generateCards = function(){
 		if(k===1){
 			var face = "J"
 			deck.push({face: face, suit:'♠'}, {face: face, suit:'♥'}, {face: face, suit:'♦'}, {face: face, suit:'♣'});
-		}
-		else if(k===2){
+		} else if(k===2){
 			var face = "Q"
                         deck.push({face: face, suit:'♠'}, {face: face, suit:'♥'}, {face: face, suit:'♦'}, {face: face, suit:'♣'});
-		}
-		else if(k===3){
+		} else if(k===3){
 			var face = "K"
                         deck.push({face: face, suit:'♠'}, {face: face, suit:'♥'}, {face: face, suit:'♦'}, {face: face, suit:'♣'});
-		}
-		else{
+		} else{
 			var face = "A"
                         deck.push({face: face, suit:'♠'}, {face: face, suit:'♥'}, {face: face, suit:'♦'}, {face: face, suit:'♣'});
 		}
 	}
 
 	return deck;
-
-	//console.log(deck);
-	console.log("There are " + deck.length + " cards in the deck.");
-
 }
 
-//REMEMBER TO REMOVE OLD CARDS USING SLICE!
 var shuffle = function(Array){
 
-	var unShuffled = generateCards();
-
-	//new deck
 	var shuffledDeck = [];
 	
 	for(var i = 0; i < 52; i++){
-		var randomCard = unShuffled[Math.floor(Math.random() * 52)];
+		var chosen = Math.floor(Math.random() * Array.length);
+		var randomCard = Array[chosen];
 		shuffledDeck.push(randomCard);
+		Array.splice(chosen, 1);
 	}
-
-	console.log("There are " + shuffledDeck.length + " cards in the shuffled deck.");
 
 	return shuffledDeck;
 
 }
 
 var calculateHand = function(Array){
-	var cards = generateCards();
 
 	var handTotal = 0;
 
-	switch(cards.face){
-		case "2":
-			handTotal += 2;
-			break;
-		case "3":
-			handTotal += 3;
-			break;
-		 case "4":
-			handTotal += 4;
-			break;
-		case "5":
-			handTotal += 5;
-			break;
-		case "6":
-			handTotal += 6;
-			break;
-		case "7":
-			handTotal += 7;
-			break;
-		case "8":
-			handTotal += 8;
-			break;
-		case "9":
-			handTotal += 9;
-			break;
-		case "A":
-			handTotal += 10;
-			if(handTotal >= 21){
-				handTotal -= 9;
-			}
-			break;
-		default:
-			handTotal += 10;
+	for(var i = 0; i < Array.length; i++){
+		switch(Array[i].face){
+			case "2":
+				handTotal += 2;
+				break;
+			case "3":
+				handTotal += 3;
+				break;
+			 case "4":
+				handTotal += 4;
+				break;
+			case "5":
+				handTotal += 5;
+				break;
+			case "6":
+				handTotal += 6;
+				break;
+			case "7":
+				handTotal += 7;
+				break;
+			case "8":
+				handTotal += 8;
+				break;
+			case "9":
+				handTotal += 9;
+				break;
+			case "A":
+				handTotal += 10;
+				if(handTotal >= 21){
+					handTotal -= 9;
+				}
+				break;
+			default:
+				handTotal += 10;
+		}
 	}
+
 	return handTotal;
 }
 
@@ -103,63 +94,87 @@ var determineWinner = function(Number1, Number2){
 	var playerTotal = Number1;
 	var computerTotal = Number2;
 
-	console.log("Your hand's total is " + playerTotal);
-	console.log("The computer's hand's total is " + computerTotal);
+	console.log("Your hand's total is " + playerTotal + ".");
+	console.log("The computer's hand's total is " + computerTotal + ".");
 
-	if(playerTotal > computerTotal && playerTotal <= 21){
-		console.log("Congratulations! You beat the computer!");
-	}
-	else if(computerTotal >= playerTotal && computerTotal <= 21){
-		console.log("Too bad, the computer beat you!");
-	}
-	else if(computerTotal === playerTotal || playerTotal > 21 && computerTotal > 21){
-		console.log("It's a tie!");
+	if(playerTotal > 21 && computerTotal > 21){
+		console.log("You and the computer bust! Sorry, no winner!");
+	}else if(playerTotal > 21 && !(computerTotal > 21)){
+		console.log("You bust! Sorry, the computer wins!");
+	}else if(!(playerTotal > 21) && computerTotal > 21){
+		console.log("The computer busts! You win!");
+	}else if(playerTotal > computerTotal){
+		console.log("You win!");
+	}else if(computerTotal > playerTotal){
+		console.log("The computer wins! Sorry!");
+	} else{
+		console.log("It's a tie! No winner!");
 	}
 }
 
 //PLAY THE GAME
 
 var deal = function(){
-	var unShuffled = generateCards();
-	var cards = shuffle(cards);
+	
+	debugger;
 
-	var playerHand = [];
-	var computerHand = [];
-
-	for(var i = 0; i < cards.length; i++){
+	var cards = shuffle(generateCards());
 		
+	while(cards.length >= 26){
+
+		console.log("New Game:");
+
+		var playerHand = [];
+		var computerHand = [];
+
 		playerHand.push(cards[0]);
 		playerHand.push(cards[1]);
+		var playerTotal = calculateHand(playerHand);
+
 		computerHand.push(cards[2]);
 		computerHand.push(cards[3]);
+		var computerTotal = calculateHand(computerHand);
+
 		cards.splice(0,4);
 
-		//player
-		var playerTotal = calculateHand(playerHand);
-		console.log("Your hand is: " + playerHand + "for a total of " + playerTotal + ".");
+		console.log("Your hand is " + playerHand[0].face + playerHand[0].suit + "  " + playerHand[1].face + playerHand[1].suit + " for a total of " + playerTotal + ".");
 		
-		if(playerTotal < 21){
-			var selection = prompt("Please choose to (h)it or (s)stay.", "");
+		while(playerTotal < 21){
+
+			var selection = prompt("Please choose 'h' to hit or 's' to stay.", "");
+
 			if(selection==="h"){
+
 				playerHand.push(cards[0]);
+				console.log("You drew a " + cards[0].face + cards[0].suit + " .");
 				cards.splice(0,1);
-			}
-			else{
+				playerTotal = calculateHand(playerHand);
+
+				if(playerTotal < 21){
+					console.log("Your new total is " + playerTotal + ".");
+				}
+
+			}else if(selection==="s"){
+
 				break;
+
+			} else{
+
+				selection = prompt("Selection not valid.", "");
+
 			}
 		}
-
-		//computer
-		var computerTotal = calculateHand(computerHand);
+		
 		while(computerTotal < 17){
 			computerHand.push(cards[0]);
 			cards.splice(0,1);
 			computerTotal = calculateHand(computerHand);
 		}
-		
+			
+		determineWinner(playerTotal, computerTotal);
+		console.log("There are " + cards.length + " cards in the deck.")
 	}
-
-	determineWinner(playerTotal, computerTotal);
+	console.log("There are less than 26 cards in the deck. Game over!");
 }
 
 deal();
